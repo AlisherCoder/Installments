94cc3be3ef6076c07a3c8b5f0b2ad97ea31f04ec
Alisher <alishersharipov670@gmail.com>
2025-06-30 02:37:18 +0500

    updated

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'nestjs-app',
-      script: 'dist/main.js',
+      script: 'dist/src/main.js',
       instances: 1,
       exec_mode: 'fork',
       env: {
